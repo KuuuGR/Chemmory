bd093e078e4f26604047eb3d0728029630a4dd8e
Grzegorz Kulesza <gkulesza@9bits.pl>
2019-03-05 09:11:24 +0100

    Add icons

--- Chemmory/Chemmory/ViewController.swift
+++ Chemmory/Chemmory/ViewController.swift
@@ -8,10 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
     @IBOutlet weak var collectionView: UICollectionView!
-    
     
     var model = CardModel()
     var cardArray = [Card]()
@@ -19,6 +18,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        collectionView.delegate = self
+        collectionView.dataSource = self
         
         //Call the getCard method of the card model
        cardArray = model.getCards()
@@ -26,5 +27,24 @@ class ViewController: UIViewController {
     }
 
 
+    // MARK: - UICollectionView Protocol Methods
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return cardArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath)
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+    
+    
 }
 
