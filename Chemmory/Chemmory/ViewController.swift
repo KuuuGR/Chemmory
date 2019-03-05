15af7c002a42d8b397c50e84aa79ce92379219e1
Grzegorz Kulesza <gkulesza@9bits.pl>
2019-03-05 15:30:17 +0100

    Add timer

--- Chemmory/Chemmory/ViewController.swift
+++ Chemmory/Chemmory/ViewController.swift
@@ -10,25 +10,55 @@ import UIKit
 
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
+    @IBOutlet weak var timerLabel: UILabel!
+    
     @IBOutlet weak var collectionView: UICollectionView!
     
     var model = CardModel()
     var cardArray = [Card]()
     
     var firstFlippedCardIndex: IndexPath?
+    
+    var timer:Timer?
+    var miliseconds:Float = 10 * 1000 // 10 seconds
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //Call the getCard method of the card model
+        cardArray = model.getCards()
+        
         collectionView.delegate = self
         collectionView.dataSource = self
         
-        //Call the getCard method of the card model
-       cardArray = model.getCards()
-       
+        // Create timer
+        timer = Timer.scheduledTimer(timeInterval: 0.001, target: self, selector: #selector(timerElapsed), userInfo: nil, repeats: true)
+        RunLoop.main.add(timer!, forMode: .common)
     }
 
-
+    override func didReceiveMemoryWarning() {
+            super.didReceiveMemoryWarning()
+            // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Timer Methods
+    
+    @objc func timerElapsed() {
+        
+        miliseconds -= 1
+        timerLabel.text?.append("A")
+        // Convert to seconds
+        let seconds = String(format: "%.2f", miliseconds/1000)
+        
+        // Set label
+        timerLabel.text = "Timer Remaining: \(seconds)"
+        
+        //When the timer reached 0
+        if miliseconds <= 0 {
+            timer?.invalidate()
+        }
+    }
+    
     // MARK: - UICollectionView Protocol Methods
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
