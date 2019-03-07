d944a05112fbae56cf3746aa9dd7d6834204cbb0
Grzegorz Kulesza <gkulesza@9bits.pl>
2019-03-07 11:36:58 +0100

    makes cards randomize

--- Chemmory/Chemmory/CardModel.swift
+++ Chemmory/Chemmory/CardModel.swift
@@ -13,35 +13,54 @@ class CardModel {
     
     func getCards() -> [Card] {
         
+        //Declare an array to store numbers we've already generated
+        var generatedNumbersArray = [Int]()
+        
         // Declare an array to store a generated cards
         var generatedCardsArray = [Card]()
         
         // Randomly generate pairs of cards
-        for _ in 1...8 {
+        //Make it so we only have inique pair of cards
+        while generatedNumbersArray.count < 8 {
             
             //Get a random number
-            let randomNumber = arc4random_uniform(12) + 1
+            let randomNumber = arc4random_uniform(13) + 1
             
-            //Log the number
-            print(randomNumber)
-            
-            //Create the first card object
-            let cardOne = Card()
-            cardOne.imageName = "card\(randomNumber)"
-            
-            generatedCardsArray.append(cardOne)
-            
-            // Create the second card object
-            let cardTwo = Card()
-            cardTwo.imageName = "card\(randomNumber)"
-            
-            generatedCardsArray.append(cardTwo)
-            
-            // OPTIONAL: Make it so we only have inique pair of cards
+            // Ensure that the number isn't one we already have
+            if generatedNumbersArray.contains(Int(randomNumber)) == false {
+               
+                //Log the number
+                print(randomNumber)
+                
+                //Store the number into generatedNumberArray
+                generatedNumbersArray.append(Int(randomNumber))
+                
+                //Create the first card object
+                let cardOne = Card()
+                cardOne.imageName = "card\(randomNumber)"
+                
+                generatedCardsArray.append(cardOne)
+                
+                // Create the second card object
+                let cardTwo = Card()
+                cardTwo.imageName = "card\(randomNumber)"
+                
+                generatedCardsArray.append(cardTwo)
+                
+            }
         }
         
         //TODO: Randomize the array
-        
+        for i in 0...generatedCardsArray.count - 1{
+            
+            // Find a random index to swap with
+            let randomNumber = Int(arc4random_uniform(UInt32(generatedCardsArray.count)))
+            
+            // Swap the two cards
+            let temporaryStorage = generatedCardsArray[i]
+            generatedCardsArray[i] = generatedCardsArray[randomNumber]
+            generatedCardsArray[randomNumber] = temporaryStorage
+        }
         // Return the array
         return generatedCardsArray
         
