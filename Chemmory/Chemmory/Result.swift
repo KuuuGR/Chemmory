c1b7fb9613d86427439d36d5b4087499eb6791dc
Grzegorz Kulesza <gkulesza@9bits.pl>
2019-03-18 13:02:08 +0100

    ad score to results

--- Chemmory/Chemmory/Result.swift
+++ Chemmory/Chemmory/Result.swift
@@ -15,5 +15,46 @@ class Result: Object {
     @objc dynamic var date: String?
     @objc dynamic var time: String?
     @objc dynamic var misses: String?
+//    @objc dynamic var score: Float?
+}
+
+class Result2: NSObject {
+    let name: String
+    let date: String
+    let time: String
+    let misses: Int
+    let score: Float
+    
+    init(name: String, date: String, time: String, misses: Int, score: Float) {
+        self.name = name
+        self.date = date
+        self.time = time
+        self.misses = misses
+        self.score = score
+        
+    }
+    
+    @objc dynamic var setOfResults: Result2?
     
 }
+
+
+
+
+/* From Stack Overflow
+
+class Time: NSObject {
+    let hours: Int
+    let minutes: Int
+    let seconds: Int
+    
+    init(hours: Int, minutes: Int, seconds: Int) {
+        self.hours = hours
+        self.minutes = minutes
+        self.seconds = seconds
+    }
+}
+
+@objc open var remainingTime: Time?
+
+ */
