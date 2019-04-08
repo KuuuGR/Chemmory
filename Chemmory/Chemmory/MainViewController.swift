22fab1f1dd448b8d81354dab5daa668257e05b61
Grzegorz Kulesza <gkulesza@9bits.pl>
2019-04-08 10:23:44 +0200

    add some graphics, start screen

--- Chemmory/Chemmory/MainViewController.swift
+++ Chemmory/Chemmory/MainViewController.swift
@@ -10,12 +10,15 @@ import UIKit
 
 class MainViewController: UIViewController {
     
+    var mPress: Int = 0
+    var rPress: Int = 0
+    
     @IBOutlet weak var startButton: UIButton!
     @IBOutlet weak var startGameLabel: UILabel!
 
     @IBOutlet weak var c_cardButton: UIButton!
     @IBOutlet weak var he_cardButton: UIButton!
-    @IBOutlet weak var m_CardButton: UIButton!
+    @IBOutlet weak var m_cardButton: UIButton!
     @IBOutlet weak var mo_cardButton: UIButton!
     @IBOutlet weak var r_cardButton: UIButton!
     @IBOutlet weak var y_cardButton: UIButton!
@@ -23,6 +26,42 @@ class MainViewController: UIViewController {
     @IBOutlet weak var right_cardButton: UIButton!
     @IBOutlet weak var bottom_cardButton: UIButton!
     
+    @IBAction func cButtonPush(_ sender: Any) {
+        
+        c_cardButton.setImage(#imageLiteral(resourceName: "card_c"), for: .normal)
+    }
+    
+    @IBAction func heButtonPush(_ sender: Any) {
+        he_cardButton.setImage(#imageLiteral(resourceName: "card_he"), for: .normal)
+    }
+    
+    @IBAction func mButtonPush(_ sender: Any) {
+       
+        mPress += 1
+        if mPress > 50 {
+            m_cardButton.setImage(#imageLiteral(resourceName: "card_M"), for: .normal)
+        }
+    }
+    
+    
+    @IBAction func moButtonPush(_ sender: Any) {
+        mo_cardButton.setImage(#imageLiteral(resourceName: "card_mo"), for: .normal)
+    }
+    
+    
+    @IBAction func rButtonPush(_ sender: Any) {
+        rPress += 1
+        
+        if rPress > 50 {
+            r_cardButton.setImage(#imageLiteral(resourceName: "card_R"), for: .normal)
+        } 
+    }
+    
+    
+    
+    @IBAction func yButtonPush(_ sender: Any) {
+        y_cardButton.setImage(#imageLiteral(resourceName: "card_y"), for: .normal)
+    }
     
     
     override func viewDidLoad() {
@@ -30,7 +69,7 @@ class MainViewController: UIViewController {
         
         prepareButton(button: c_cardButton)
         prepareButton(button: he_cardButton)
-        prepareButton(button: m_CardButton)
+        prepareButton(button: m_cardButton)
         prepareButton(button: mo_cardButton)
         prepareButton(button: r_cardButton)
         prepareButton(button: y_cardButton)
@@ -39,19 +78,34 @@ class MainViewController: UIViewController {
         prepareButton(button: bottom_cardButton)
 
         startButton.layer.backgroundColor = UIColor.red.cgColor
-        startButton.layer.cornerRadius = 60 //40//startButton.bounds.size.height * 0.55
+        startButton.layer.cornerRadius = 60
         
         startButton.layer.borderWidth = 8
         startButton.layer.borderColor = UIColor.white.cgColor
         startGameLabel.textColor = UIColor.yellow
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        if mPress >= 1 {
+            mPress = 0
+            m_cardButton.setImage(#imageLiteral(resourceName: "card_m1"), for: .normal)
+        }
+        if rPress >= 1 {
+            rPress = 0
+            r_cardButton.setImage(#imageLiteral(resourceName: "card_R1"), for: .normal)
+        }
+        
+        super.viewDidAppear(true)
+    }
+    
+    
     func prepareButton(button: UIButton) {
-        button.layer.backgroundColor = UIColor.red.cgColor
+        button.layer.backgroundColor = UIColor.orange.cgColor
         button.layer.cornerRadius = button.bounds.size.height * 0.1
-        button.alpha = 0.1
+        button.alpha = 0.4
         button.layer.borderWidth = 2
         button.layer.borderColor = UIColor.white.cgColor
+        button.clipsToBounds = true
     }
     
 }
