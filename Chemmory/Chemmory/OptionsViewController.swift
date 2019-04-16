128e697ef1214f40fdead8ec771bb1f8adef2fc9
Grzegorz Kulesza <gkulesza@9bits.pl>
2019-04-16 08:25:14 +0200

    options scroll view

---
+++ Chemmory/Chemmory/OptionsViewController.swift
@@ -0,0 +1,34 @@
+//
+//  OptionsViewController.swift
+//  Chemmory
+//
+//  Created by Grzesiek Kulesza on 16/04/2019.
+//  Copyright © 2019 Grzesiek Kulesza. All rights reserved.
+//
+
+import UIKit
+
+class OptionsViewController: UIViewController {
+
+    @IBOutlet weak var ScrollView: UIScrollView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //ScrollView.contentSize.height = 1000
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
