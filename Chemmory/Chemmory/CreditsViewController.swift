3a39770e3549ddf81d1816f65bb0c0c5a129ca32
Grzegorz Kulesza <gkulesza@9bits.pl>
2019-05-07 08:24:54 +0200

    add 9bits logo

--- Chemmory/Chemmory/CreditsViewController.swift
+++ Chemmory/Chemmory/CreditsViewController.swift
@@ -10,15 +10,26 @@ import UIKit
 
 class CreditsViewController: UIViewController {
 
+    @IBOutlet weak var logoButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        logoButton.layer.cornerRadius =  logoButton.frame.height * 0.1
+        logoButton.layer.borderWidth = 4
+        logoButton.layer.borderColor = UIColor.darkGray.cgColor
+        logoButton.clipsToBounds = true
     }
     
-    @IBAction func BackButtonPressed(_ sender: UIButton) {
+    @IBAction func backButtonTapped(_ sender: UIButton) {
         self.navigationController?.popViewController(animated: true)
     }
+    
+    @IBAction func logoButtonTapped(_ sender: Any) {
+        
+    }
+    
     
     /*
     // MARK: - Navigation
