b026a9b284d6e6ecda4195cbbd5aa49d2528df55
Grzegorz Kulesza <gkulesza@9bits.pl>
2019-05-14 09:58:09 +0200

    add animation

--- Chemmory/Chemmory/CreditsViewController.swift
+++ Chemmory/Chemmory/CreditsViewController.swift
@@ -8,18 +8,29 @@
 
 import UIKit
 
+let durationAnimation: Double = 3.5
+
 class CreditsViewController: UIViewController {
 
     @IBOutlet weak var logoButton: UIButton!
-    
+    @IBOutlet weak var upperConstraint: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         logoButton.layer.cornerRadius =  logoButton.frame.height * 0.1
         logoButton.layer.borderWidth = 4
         logoButton.layer.borderColor = UIColor.darkGray.cgColor
         logoButton.clipsToBounds = true
+        
+        logoButton.rotate()
+        UIView.animate(withDuration: durationAnimation, delay: 0, options: .curveEaseOut, animations: {
+            self.upperConstraint.constant += self.view.bounds.height/5
+            self.view.layoutIfNeeded()
+            
+        }, completion: { (true) in //self.animationLoadCycle()
+            self.logoButton.stopRotating()
+            })
+       // upperConstraint.constant = 120.0
     }
     
     @IBAction func backButtonTapped(_ sender: UIButton) {
@@ -28,17 +39,30 @@ class CreditsViewController: UIViewController {
     
     @IBAction func logoButtonTapped(_ sender: Any) {
         
+        UIApplication.shared.openURL(URL(string: "https://www.9bits.com/")!)
     }
     
+}
+
+extension UIView {
+    private static let kRotationAnimationKey = "rotationanimationkey"
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func rotate(duration: Double = durationAnimation/2) {
+        if layer.animation(forKey: UIView.kRotationAnimationKey) == nil {
+            let rotationAnimation = CABasicAnimation(keyPath: "transform.rotation")
+            
+            rotationAnimation.fromValue = 0.0
+            rotationAnimation.toValue = Float.pi * 2.0
+            rotationAnimation.duration = duration
+            rotationAnimation.repeatCount = Float.infinity
+            
+            layer.add(rotationAnimation, forKey: UIView.kRotationAnimationKey)
+        }
     }
-    */
-
+    
+    func stopRotating() {
+        if layer.animation(forKey: UIView.kRotationAnimationKey) != nil {
+            layer.removeAnimation(forKey: UIView.kRotationAnimationKey)
+        }
+    }
 }
