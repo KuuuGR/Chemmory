f19a482b1efe6dbe2a8dd4455e1bff4f013c23a6
Grzegorz Kulesza <gkulesza@9bits.pl>
2019-07-01 15:26:53 +0200

    highScore view update

--- Chemmory/Chemmory/HighScoreViewController.swift
+++ Chemmory/Chemmory/HighScoreViewController.swift
@@ -52,24 +52,126 @@ class HighScoreViewController: UIViewController, UITableViewDelegate, UITableVie
         // prepare text for cell
         let realm = try! Realm()
         let grabResults = realm.objects(Result.self).sorted(byKeyPath: "score", ascending: false)
-        var textForCell = ""
+//        var textForCell = ""
         
         let positionStr = String(indexPath.row + 1)
         let nameStr = grabResults[indexPath.row].name ?? "noname"
         
-        let timeStr = String(grabResults[indexPath.row].time)
+        let timeStr = String(format: "%.0f", grabResults[indexPath.row].time)
         let dayStr = grabResults[indexPath.row].date ?? "noday"
         
-        let scoreStr = String(grabResults[indexPath.row].score)
-        let hintStr = grabResults[indexPath.row].hint ?? "unknown hint"
+        let scoreStr = String(format: "%.0f", grabResults[indexPath.row].score)
+        let hintStr = hintToSymbol(grabResults[indexPath.row].hint ?? "unknown hint")
         
-        //TODO: make score and time aproximate, make hint as a Emoji, add tekst hint:, time:, date,
-        // add localized names
+        //TODO: add localized names
         
-        textForCell.append("\(positionStr). \(nameStr) \(timeStr) \(dayStr) \(scoreStr) \(hintStr)")
+//        textForCell.append("\(positionStr). User: \(nameStr); score: \(scoreStr); time: \(timeStr)s; hint: \(hintStr); play at: \(dayStr)")
         
-        cell.textLabel!.text = textForCell
+        //let fontSize = 15.0 as CGFloat
+        let fontSize = returnFontSize()
         
+        
+        let attributedString = NSMutableAttributedString(string: "",
+                                                  attributes:[NSAttributedString.Key.foregroundColor: UIColor.lightGray,
+                                                              NSAttributedString.Key.font: UIFont(name: "Arial", size: fontSize) as Any])
+        
+        let attStr01 = NSMutableAttributedString(string: "\(positionStr). ",
+            attributes:[NSAttributedString.Key.foregroundColor: UIColor.gray,
+                        NSAttributedString.Key.font: UIFont(name: "AvenirNext-Bold", size: fontSize) as Any])
+        attributedString.append(attStr01)
+       
+//        let attStr02 = NSMutableAttributedString(string: "user: ",
+//            attributes:[NSAttributedString.Key.foregroundColor: UIColor.lightGray,
+//                        NSAttributedString.Key.font: UIFont(name: "Chalkduster", size: 10.0) as Any])
+//        attributedString.append(attStr02)
+        
+        let attStr03 = NSMutableAttributedString(string: "\(nameStr) ",
+            attributes:[NSAttributedString.Key.foregroundColor: UIColor.black,
+                        NSAttributedString.Key.font: UIFont(name: "AvenirNext-Bold", size: fontSize) as Any])
+        attributedString.append(attStr03)
+        
+        let attStr04 = NSMutableAttributedString(string: "score: ",
+            attributes:[NSAttributedString.Key.foregroundColor: UIColor.lightGray,
+                        NSAttributedString.Key.font: UIFont(name: "Chalkduster", size: fontSize) as Any])
+        attributedString.append(attStr04)
+        
+        let attStr05 = NSMutableAttributedString(string: "\(scoreStr) pts. ",
+            attributes:[NSAttributedString.Key.foregroundColor: UIColor.black,
+                        NSAttributedString.Key.font: UIFont(name: "AvenirNext-Bold", size: fontSize) as Any])
+        attributedString.append(attStr05)
+        
+        let attStr06 = NSMutableAttributedString(string: "time: ",
+            attributes:[NSAttributedString.Key.foregroundColor: UIColor.lightGray,
+                        NSAttributedString.Key.font: UIFont(name: "Chalkduster", size: fontSize) as Any])
+        attributedString.append(attStr06)
+        
+        let attStr07 = NSMutableAttributedString(string: "\(timeStr) s ",
+            attributes:[NSAttributedString.Key.foregroundColor: UIColor.black,
+                        NSAttributedString.Key.font: UIFont(name: "AvenirNext-Bold", size: fontSize) as Any])
+        attributedString.append(attStr07)
+        
+        let attStr08 = NSMutableAttributedString(string: "\nhint: ",
+            attributes:[NSAttributedString.Key.foregroundColor: UIColor.lightGray,
+                        NSAttributedString.Key.font: UIFont(name: "Chalkduster", size: fontSize) as Any])
+        attributedString.append(attStr08)
+        
+        let attStr09 = NSMutableAttributedString(string: "\(hintStr) ",
+            attributes:[NSAttributedString.Key.foregroundColor: UIColor.lightGray,
+                        NSAttributedString.Key.font: UIFont(name: "Arial", size: 2 * fontSize) as Any])
+        attributedString.append(attStr09)
+        
+        let attStr10 = NSMutableAttributedString(string: "play at: ",
+            attributes:[NSAttributedString.Key.foregroundColor: UIColor.lightGray,
+                        NSAttributedString.Key.font: UIFont(name: "Chalkduster", size: fontSize) as Any])
+        attributedString.append(attStr10)
+        
+        let attStr11 = NSMutableAttributedString(string: "\(dayStr) ",
+                                                 attributes:[NSAttributedString.Key.foregroundColor: UIColor.gray,
+                                                             NSAttributedString.Key.font: UIFont(name: "AvenirNext-Bold", size: fontSize) as Any])
+        attributedString.append(attStr11)
+        
+
+        
+        //cell.textLabel!.text = textForCell
+        cell.textLabel!.attributedText = attributedString
+        cell.textLabel?.numberOfLines = 0
         return cell
     }
+    
+    func hintToSymbol(_ hint: String) -> String {
+        
+        switch hint {
+        case "mass":
+            return "🐘"
+        //return "M"
+        case "number":
+            return "🐠"
+        //return "N"
+        case "valence":
+            return "🐉"
+        //return "V"
+        case "name":
+            return "🐓"
+        //return "n"
+        case "symbol":
+            return "🐬"
+        //return " "
+        default:
+            return "🐛"
+        }
+        
+    }
+    
+    func returnFontSize() -> CGFloat {
+        let width = UIScreen.main.bounds.width
+        let height = UIScreen.main.bounds.height
+        // for iPhone X
+        if (width == 812 && height == 375) {
+            return 14 as CGFloat
+        }
+        // other iPhones
+        return ((18 * width)/667) as CGFloat
+        
+    }
+    
 }
