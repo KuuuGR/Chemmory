d8b009bbdad899c0995ce27511bc55afb29bba89
Grzegorz Kulesza <gkulesza@9bits.pl>
2019-08-06 14:41:49 +0200

    improvement of the turn order

--- Chemmory/Chemmory/HiddenGameViewController.swift
+++ Chemmory/Chemmory/HiddenGameViewController.swift
@@ -14,13 +14,21 @@ class HiddenGameViewController: UIViewController {
     @IBOutlet var playButtons: [UIButton]!
     @IBOutlet weak var scoreLabel: UILabel!
     
-    var memorySequenceArray: [Int] = [1,1,1,1,1,2,3,3]
-    
+    var memorySequenceArray: [Int] = []
+    var playerMemorySequenceArray: [Int] = []
     var timer:Timer?
     var milliseconds:Float = 0
     var showingSpeed: Float = 0.01
     
     var greyBorder: Bool = false
+    
+//    var isBorderColor0: Bool = false
+//    var isBorderColor1: Bool = false
+//    var isBorderColor2: Bool = false
+//    var isBorderColor3: Bool = false
+//    var isBorderColor4: Bool = false
+//    var isBorderColor5: Bool = false
+        var playing = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,18 +37,52 @@ class HiddenGameViewController: UIViewController {
         
     }
     
-    
     @IBAction func gameBoardButtonTapped(_ sender: UIButton) {
-       
+        
+//        guard (memorySequenceArray.count == playerMemorySequenceArray.count + 1) else {return scoreLabel.text = "outOfRage"}
+//            playerMemorySequenceArray.append(sender.tag)
+//            for i in 0..<playerMemorySequenceArray.count - 1 {
+//                if playerMemorySequenceArray[i] != memorySequenceArray [i] {
+//                 scoreLabel.text = "BAD"
+//                } else {nextTurn()}
+//                scoreLabel.text = "Good"
+//            }
+//            makeButtonColor(sender.tag)
+        
+        timer?.invalidate()
+        allButtonsWhite()
         makeButtonColor(sender.tag)
+        
+        if playing == true {
+        playerMemorySequenceArray.append(sender.tag)
+        }
+            
+        if (memorySequenceArray.count == playerMemorySequenceArray.count), (playerMemorySequenceArray.count > 0)  {
+            for i in 0...memorySequenceArray.count - 1 {
+                if playerMemorySequenceArray[i] != memorySequenceArray [i] {
+                    scoreLabel.text = "BAD"
+                    resetGame()
+                    playing = false
+                    break
+                }
+            }
+            
+            nextTurn()
+            scoreLabel.text = "Good"
+        }
+        
         
     }
     
     
     @IBAction func startButtonTapped(_ sender: Any) {
-        
-        scoreLabel.text = ""
-        nextTurn()
+            resetGame()
+        guard milliseconds == 0 else {return}
+            //timer?.invalidate()
+            //resetGame()
+            playing = true
+            scoreLabel.text = ""
+            nextTurn()
     }
     
     @IBAction func backButtonTaped(_ sender: UIButton) {
@@ -61,6 +103,9 @@ class HiddenGameViewController: UIViewController {
     
     func nextTurn() {
         // TODO keep buttons not clicable
+        //timer?.invalidate()
+        playing = true
+        playerMemorySequenceArray = []
         memorySequenceArray.append(Int.random(in: 0 ... 4))
         milliseconds = 0
         showingSpeed = 0.01
@@ -74,25 +119,44 @@ class HiddenGameViewController: UIViewController {
         switch tag {
         case 1:
             playButtons[tag].backgroundColor = UIColor.cyan
+//            isBorderColor1 == true ? (playButtons[tag].layer.borderColor = UIColor.cyan.cgColor) : (playButtons[tag].layer.borderColor = UIColor.white.cgColor)
+//            isBorderColor1 = !isBorderColor1
         case 2:
             playButtons[tag].backgroundColor = UIColor.green
+//            isBorderColor2 == true ? (playButtons[tag].layer.borderColor = UIColor.green.cgColor) : (playButtons[tag].layer.borderColor = UIColor.white.cgColor)
+//            isBorderColor2 = !isBorderColor2
         case 3:
             playButtons[tag].backgroundColor = UIColor.blue
+//            isBorderColor3 == true ? (playButtons[tag].layer.borderColor = UIColor.blue.cgColor) : (playButtons[tag].layer.borderColor = UIColor.white.cgColor)
+//            isBorderColor3 = !isBorderColor3
         case 4:
             playButtons[tag].backgroundColor = UIColor.red
+//            isBorderColor4 == true ? (playButtons[tag].layer.borderColor = UIColor.red.cgColor) : (playButtons[tag].layer.borderColor = UIColor.white.cgColor)
+//            isBorderColor4 = !isBorderColor4
         default:
             playButtons[tag].backgroundColor = UIColor.yellow
+//            isBorderColor0 == true ? (playButtons[tag].layer.borderColor = UIColor.yellow.cgColor) : (playButtons[tag].layer.borderColor = UIColor.white.cgColor)
+//            isBorderColor0 = !isBorderColor0
         }
     }
     
     func prepareGameBoardButtons() {
         for button in playButtons {
-            //button.backgroundColor = .clear
             button.layer.cornerRadius = 10
             button.layer.borderWidth = 3
             button.layer.borderColor = UIColor.white.cgColor
         }
         
+    }
+    
+    func resetGame() {
+        
+        playerMemorySequenceArray = []
+        memorySequenceArray = []
+        milliseconds = 0
+        showingSpeed = 0.01
+        allButtonsWhite()
+        timer?.invalidate()
     }
     
     // MARK: - Timer Methods
@@ -102,20 +166,21 @@ class HiddenGameViewController: UIViewController {
         let milInt = Int(milliseconds)
 
         scoreLabel.text = String(milliseconds)
-        allButtonsWhite()
         
         let actualSequenceTag: Int = milInt % memorySequenceArray.count
         
-        
         // make grey borderColor
         
-        if memorySequenceArray.count > 1, actualSequenceTag > 0, memorySequenceArray[actualSequenceTag] ==  memorySequenceArray[actualSequenceTag - 1], !greyBorder {
-            greyBorder = !greyBorder
-        } else if greyBorder == true { greyBorder = false}
-        else { greyBorder = false}
+//        if memorySequenceArray.count > 1, actualSequenceTag > 0, memorySequenceArray[actualSequenceTag] ==  memorySequenceArray[actualSequenceTag - 1], !greyBorder {
+//            greyBorder = true
+//        } else if greyBorder == true { greyBorder = false}
+//        else { greyBorder = false}
         
+        allButtonsWhite()
         makeButtonColor(memorySequenceArray[actualSequenceTag], isGreyBorderColor: greyBorder)
-        
+        if memorySequenceArray.count == actualSequenceTag {
+            milliseconds = 0
+        }
         //When the timer reached max
         if milInt >= memorySequenceArray.count {
             // Stop the timer
