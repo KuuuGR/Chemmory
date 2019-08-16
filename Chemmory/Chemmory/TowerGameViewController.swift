8332a50dc53c2b54b7836df783f45da3d965026b
Grzegorz Kulesza <gkulesza@9bits.pl>
2019-08-16 11:43:30 +0200

    prepare buttons view

--- Chemmory/Chemmory/TowerGameViewController.swift
+++ Chemmory/Chemmory/TowerGameViewController.swift
@@ -13,11 +13,12 @@ class TowerGameViewController: UIViewController {
     var picNumber = 0
     @IBOutlet weak var towerImageView: UIImageView!
     @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet var answerButons: [UIButton]!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        prepareButtons()
 
         // Do any additional setup after loading the view.
     }
@@ -31,11 +32,34 @@ class TowerGameViewController: UIViewController {
     @IBAction func button5of5tapped(_ sender: UIButton) {
         let imageName = "puzzle" + String(picNumber) + ".png"
         towerImageView.image = UIImage(named: imageName)
-        //towerImageView.image = UIImage(contentsOfFile: imageName)
         picNumber += 1
     }
     
+    @IBAction func answerButtoTapped(_ sender: UIButton) {
+        
+    }
     
+    
+    func prepareButtons(){
+        // round corners in image of the tower
+        towerImageView.layer.cornerRadius = 16.0
+        towerImageView.clipsToBounds = true
+        
+        // round corners and prepare answer buttons
+        for button in answerButons {
+            button.backgroundColor = .clear
+            button.layer.cornerRadius = 15
+            button.layer.borderWidth = 3
+            button.layer.borderColor = UIColor.white.cgColor
+        }
+        
+        // round corners question laber
+        questionLabel.layer.cornerRadius = 15
+        questionLabel.layer.borderColor = UIColor.white.cgColor
+        questionLabel.layer.borderWidth = 3
+        questionLabel.layer.masksToBounds = true
+        
+    }
     
     /*
     // MARK: - Navigation
