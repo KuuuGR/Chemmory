76526c74f9272573d3d91e23ddffacc12c473ce7
Grzegorz Kulesza <gkulesza@9bits.pl>
2019-08-16 15:15:10 +0200

    Add sound and shows answers as symbols

--- Chemmory/Chemmory/TowerGameViewController.swift
+++ Chemmory/Chemmory/TowerGameViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 
 class TowerGameViewController: UIViewController {
 
-    var picNumber = 0
+    var questionNumber = 1
+    var answer: [Int] = [0,0,0,0,0]
     @IBOutlet weak var towerImageView: UIImageView!
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet var answerButons: [UIButton]!
@@ -18,25 +19,29 @@ class TowerGameViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         prepareButtons()
-
-        // Do any additional setup after loading the view.
+        
+        answer = prepareRandomAnswers(questionNumber, answer, 112)
+        //signAnswerButtonsValues()
+        setAnswerButtonsChemicalNames()
     }
-    
 
     @IBAction func backButtonTapped(_ sender: UIButton) {
         SoundManager.playSound(.back)
         self.navigationController?.popViewController(animated: true)
     }
     
-    @IBAction func button5of5tapped(_ sender: UIButton) {
-        let imageName = "puzzle" + String(picNumber) + ".png"
-        towerImageView.image = UIImage(named: imageName)
-        picNumber += 1
-    }
     
     @IBAction func answerButtoTapped(_ sender: UIButton) {
-        
+       
+        answer[sender.tag] == questionNumber ? (SoundManager.playSound(.down)) : (SoundManager.playSound(.results))
+        answer[sender.tag] == questionNumber ? (questionNumber += 1) : (questionNumber = 1)
+        questionLabel.text = String(questionNumber)
+        answer = prepareRandomAnswers(questionNumber, answer, 112)
+        //signAnswerButtonsValues()
+        setAnswerButtonsChemicalNames()
+        showPartPicture(questionNumber, 112)
     }
     
     
@@ -61,14 +66,39 @@ class TowerGameViewController: UIViewController {
         
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func showPartPicture(_ currntValue: Int,_ maxValue: Int) {
+        let picNumber = Float(currntValue)/Float(maxValue) * 24
+        let imageName = "puzzle" + String(Int(picNumber)) + ".png"
+        towerImageView.image = UIImage(named: imageName)
     }
-    */
-
+    
+    func prepareRandomAnswers(_ trueValue: Int, _ answers: [Int], _ maxAnswer: Int) -> [Int] {
+        var newAnswers = answers
+        var randomInt = 1
+        newAnswers[0] = trueValue
+        
+        // TODO: remove repetition of elements
+        for i in 1...4 {
+            repeat {
+                randomInt = Int.random(in: 1...maxAnswer)
+                newAnswers[i] = randomInt
+            } while !(newAnswers.contains(randomInt))
+        }
+        
+        newAnswers = newAnswers.shuffled()
+        return newAnswers
+    }
+    
+    func signAnswerButtonsValues(){
+        for button in answerButons {
+            button.setTitle(String(answer[button.tag]), for: .normal)
+        }
+    }
+    
+    func setAnswerButtonsChemicalNames(){
+        for button in answerButons {
+            button.setTitle(elements[answer[button.tag]].symbol, for: .normal)
+        }
+    }
+ 
 }
