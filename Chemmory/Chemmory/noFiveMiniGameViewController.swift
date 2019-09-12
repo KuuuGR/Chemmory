bbb77972b37c2a0e9755a5cdba1e151e18089949
Grzegorz Kulesza <gkulesza@9bits.pl>
2019-09-12 15:07:24 +0200

    graphics and some logic of game

--- Chemmory/Chemmory/noFiveMiniGameViewController.swift
+++ Chemmory/Chemmory/noFiveMiniGameViewController.swift
@@ -11,26 +11,81 @@ import UIKit
 class noFiveMiniGameViewController: UIViewController {
 
     @IBOutlet weak var centralSaveButton: UIButton!
+    @IBOutlet weak var roundLabel: UILabel!
+    @IBOutlet weak var winLabel: UILabel!
     
+    @IBOutlet weak var medal1: UIImageView!
+    @IBOutlet weak var medal2: UIImageView!
+    @IBOutlet weak var medal3: UIImageView!
+    @IBOutlet weak var medal4: UIImageView!
+    
+    @IBOutlet var allButtons: [UIButton]!
+    
+    var question: [Int] = [0,0,0,0,0,0,0,0,0,0,0,0]
+    var winCounter: Int = 0
+    var roundCounter: Int = 0
+    var answer: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         prepareButton(centralSaveButton)
-        // Do any additional setup after loading the view.
+        
+        
+        for button in allButtons { prepareButton(button) }
+ 
+        answer = Int.random(in: 1...112)
+        question = prepareRandomAnswers(answer, 112)
+        setButtonsValues()
+        let buttonChoosen = showOnlyRandomOne()
+        answer = question[buttonChoosen]
+        print(answer)
+
     }
     
     
     @IBAction func centralSaveButton(_ sender: UIButton) {
         SoundManager.playSound(.up)
+        roundCounter += 1
+        
+        refreshScoreLabels()
+        
+        showAllButtons()
+        centralSaveButton.isEnabled = false
+        
+        question = prepareRandomAnswers(answer, 112)
+        setButtonsValues()
+        print(answer)
+        
     }
     
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        if question[sender.tag] == answer {
+            SoundManager.playSound(.correct)
+            winCounter += 1
+            showAwards(winCounter)
+        }
+        refreshScoreLabels()
+    }
     
     @IBAction func backButtonPressed(_ sender: UIButton) {
         SoundManager.playSound(.back)
         self.navigationController?.popViewController(animated: true)
     }
     
+    func setButtonsValues (){
+        for button in allButtons {
+            setButtonsLabels(button, question[button.tag])
+        }
+    }
+    
+    func setButtonsLabels (_ button: UIButton, _ attomicNumber: Int) {
+        
+        button.titleLabel?.lineBreakMode = .byWordWrapping
+        let massText = String(format: "%.02f", elements[attomicNumber].atomicMass)
+        let symbolText = elements[attomicNumber].symbol
+        button.setTitle("\(attomicNumber)\n    \(symbolText)\n  \(massText)", for: .normal)
+    }
     
     func prepareButton(_ button: UIButton){
         
@@ -41,6 +96,64 @@ class noFiveMiniGameViewController: UIViewController {
         button.setTitleColor(.white, for: .normal)
        
     }
+    
+    func showOnlyRandomOne() -> Int {
+        
+        let randBut = Int.random(in: 0...11)
+        
+        for button in allButtons {
+            if button.tag != randBut {
+                button.isHidden = true
+            }
+                button.isEnabled = false
+        }
+        
+        return randBut
+    }
+    
+    func prepareRandomAnswers(_ correctAnswer: Int,_ maxAnswer: Int) -> [Int] {
+        let numberOfAnswers = 12
+        var answers: Set<Int> = [correctAnswer]
+        while answers.count < numberOfAnswers {
+            answers.insert(Int.random(in: 1...maxAnswer))
+        }
+        return answers.shuffled()
+    }
+    
+    func showAllButtons() {
+        for button in allButtons{
+            button.isHidden = false
+            button.isEnabled = true
+        }
+        
+    }
+    
+    func refreshScoreLabels() {
+        winLabel.text = String(winCounter)
+        roundLabel.text = String(roundCounter)
+    }
+    
+    func showAwards(_ wins: Int){
+        
+        switch wins {
+        case 1...10:
+            medal1.isHidden = false
+        case 11...50:
+            medal2.isHidden = false
+        case 51...400:
+            medal3.isHidden = false
+        case 401...1000:
+            medal4.isHidden = false
+        case 1001...2000:
+            centralSaveButton.setImage(#imageLiteral(resourceName: "miniVSaveGold"), for: .normal)
+        case 2001...5000:
+            centralSaveButton.setImage(#imageLiteral(resourceName: "miniVSaveClock"), for: .normal)
+        case 5000...20000:
+            centralSaveButton.setImage(#imageLiteral(resourceName: "rinbowStar"), for: .normal)
+        default:
+            return
+        }
 
+    }
 
 }
